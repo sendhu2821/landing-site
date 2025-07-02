7771f45b9c30700403905519c72c127115fced93
sendhu <senthamizh764@gmail.com>
2025-07-02 17:12:22 +0530

    Implement About, Art and Cocktails components and add masked-image in Art section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ScrollTrigger, SplitText } from "gsap/all";
 
 import NavBar from "./components/NavBar.jsx";
 import Hero from "./components/Hero.jsx";
+import Cocktails from "./components/Cocktails.jsx";
+import About from "./components/About.jsx";
+import Art from "./components/Art.jsx";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -11,6 +14,9 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
+      <Cocktails />
+      <About />
+      <Art />
     </main>
   );
 };
