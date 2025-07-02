f7a9b1ce21a5cdd0795dcf976326b14aa307f657
sendhu <senthamizh764@gmail.com>
2025-07-02 13:40:51 +0530

    Implement Nav and Hero, with scroll trigger animation for Background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-function App() {
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
   return (
-    <>
-      <div>
-        <h1>Hello! Again</h1>
-      </div>
-    </>
+    <main>
+      <NavBar />
+      <Hero />
+    </main>
   );
-}
+};
 
 export default App;
